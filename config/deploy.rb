b13fe7929f450589a2feafd46c7ad4e50cccaa1e
WilliamSinclairF <willzy@outlook.com>
2020-11-21 00:49:58 -0500

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '~> 3.14.1'
 
 before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, %w[rake gem bundle ruby puma pumactl]
-set :application, 'WilliamSinclair'
+set :application, 'team_will_sinclair'
 set :repo_url, 'git@github.com:WilliamSinclairF/Rocket-Elevator-Foundation.git'
 
 # Default branch is :master
