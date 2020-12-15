40545b90b2dae2662fcfbb9a4692c49954502232
Jcoderre <joeysteam@outlook.com>
2020-12-15 16:16:00 -0500

    add speech2text and speaker recognition

---
+++ conversation_transcription.js
@@ -0,0 +1,96 @@
+const fs = require('fs');
+const axios = require('axios');
+const formData = require('form-data');
+ 
+const subscriptionKey = 'a9e36bea7d01419f8920661a2721764e';
+const region = 'westus';
+ 
+async function createProfile() {
+    let form = new formData();
+    form.append('file', fs.createReadStream('path-to-voice-sample.wav'));
+    let headers = form.getHeaders();
+    headers['a9e36bea7d01419f8920661a2721764e'] = subscriptionKey;
+ 
+    let url = `https://signature.${region}.cts.speech.microsoft.com/api/v1/Signature/GenerateVoiceSignatureFromFormData`;
+    let response = await axios.post(url, form, { headers: headers });
+    
+    // get signature from response, serialize to json string
+    return JSON.stringify(response.data.Signature);
+}
+ 
+async function main() {
+    // use this voiceSignature string with conversation transcription calls below
+    let voiceSignatureString = await createProfile();
+    console.log(voiceSignatureString);
+}
+main();
+
+(function() {
+    "use strict";
+    var sdk = require("microsoft-cognitiveservices-speech-sdk");
+    var fs = require("fs");
+    
+    var subscriptionKey = "your-subscription-key";
+    var region = "your-region";
+    var filepath = "audio-file-to-transcribe.wav"; // 8-channel audio
+    
+    // create the push stream and write file to it
+    var pushStream = sdk.AudioInputStream.createPushStream();
+    fs.createReadStream(filepath).on('data', function(arrayBuffer) {
+        pushStream.write(arrayBuffer.slice());
+    }).on('end', function() {
+        pushStream.close();
+    });
+    
+    var speechTranslationConfig = sdk.SpeechTranslationConfig.fromSubscription(subscriptionKey, region);
+    var audioConfig = sdk.AudioConfig.fromStreamInput(pushStream);
+    speechTranslationConfig.speechRecognitionLanguage = "en-US";
+    
+    // create conversation and transcriber
+    var conversation = sdk.Conversation.createConversationAsync(speechTranslationConfig, "myConversation");
+    var transcriber = new sdk.ConversationTranscriber(audioConfig);
+    
+    // attach the transcriber to the conversation
+    transcriber.joinConversationAsync(conversation,
+    function () {
+        // add first participant using voiceSignature created in enrollment step
+        var user1 = sdk.Participant.From("user1@example.com", "en-us", voiceSignatureStringUser1);
+        conversation.addParticipantAsync(user1,
+        function () {
+            // add second participant using voiceSignature created in enrollment step
+            var user2 = sdk.Participant.From("user2@example.com", "en-us", voiceSignatureStringUser2);
+            conversation.addParticipantAsync(user2,
+            function () {
+                transcriber.sessionStarted = function(s, e) {
+                console.log("(sessionStarted)");
+                };
+                transcriber.sessionStopped = function(s, e) {
+                console.log("(sessionStopped)");
+                };
+                transcriber.canceled = function(s, e) {
+                console.log("(canceled)");
+                };
+                transcriber.transcribed = function(s, e) {
+                console.log("(transcribed) text: " + e.result.text);
+                console.log("(transcribed) speakerId: " + e.result.speakerId);
+                };
+    
+                // begin conversation transcription
+                transcriber.startTranscribingAsync(
+                function () { },
+                function (err) {
+                    console.trace("err - starting transcription: " + err);
+                });
+        },
+        function (err) {
+            console.trace("err - adding user1: " + err);
+        });
+    },
+    function (err) {
+        console.trace("err - adding user2: " + err);
+    });
+    },
+    function (err) {
+    console.trace("err - " + err);
+    });
+}());
