3cbec7b943e67b37d05d922b18119d0fbba25c88
trevorius <trevorius45@hotmail.com>
2020-12-15 19:08:37 -0500

    createProfile.js

--- speakerRecognition.js
+++ speakerRecognition.js
@@ -3,6 +3,8 @@
     */
 require('dotenv').config();
 
+var createProfile = require('./newProfile.js');
+
 const { Console } = require("console");
 const { ConnectionEstablishErrorEvent } = require("microsoft-cognitiveservices-speech-sdk/distrib/lib/src/common/Exports");
 
@@ -201,16 +203,105 @@ recognizer.recognizeOnceAsync(
 // </code>
 
 };
-    
+
+
+
+
+
+
+
+
+
+
+function transcribe(){
+    var subscriptionKey = process.env.key_var;
+    var region2 = process.env.region_var;
+    var filepath = "sounds/vampIntroTrevor.wav"; // 8-channel audio
+
+    // create the push stream and write file to it
+    var pushStream = sdk.AudioInputStream.createPushStream();
+    fs.createReadStream(filepath).on('data', function(arrayBuffer) {
+        pushStream.write(arrayBuffer.slice());
+    }).on('end', function() {
+        pushStream.close();
+    });
+
+    var speechTranslationConfig = sdk.SpeechTranslationConfig.fromSubscription(subscriptionKey, region2);
+    var audioConfig = sdk.AudioConfig.fromStreamInput(pushStream);
+    speechTranslationConfig.speechRecognitionLanguage = "en-US";
+
+    // create conversation and transcriber
+    var conversation = sdk.Conversation.createConversationAsync(speechTranslationConfig, "myConversation");
+    var transcriber = new sdk.ConversationTranscriber(audioConfig);
+
+    // attach the transcriber to the conversation
+    transcriber.joinConversationAsync(conversation,
+    function () {
+        // add first participant using voiceSignature created in enrollment step
+        var user1 = sdk.Participant.From("user1@example.com", "en-us", voiceSignatureStringUser1);
+        conversation.addParticipantAsync(user1,
+        function () {
+            // add second participant using voiceSignature created in enrollment step
+            var user2 = sdk.Participant.From("user2@example.com", "en-us", voiceSignatureStringUser2);
+            conversation.addParticipantAsync(user2,
+            function () {
+                transcriber.sessionStarted = function(s, e) {
+                console.log("(sessionStarted)");
+                };
+                transcriber.sessionStopped = function(s, e) {
+                console.log("(sessionStopped)");
+                };
+                transcriber.canceled = function(s, e) {
+                console.log("(canceled)");
+                };
+                transcriber.transcribed = function(s, e) {
+                console.log("(transcribed) text: " + e.result.text);
+                console.log("(transcribed) speakerId: " + e.result.speakerId);
+                };
+
+                // begin conversation transcription
+                transcriber.startTranscribingAsync(
+                function () { },
+                function (err) {
+                    console.trace("err - starting transcription: " + err);
+                });
+        },
+        function (err) {
+            console.trace("err - adding user1: " + err);
+        });
+    },
+    function (err) {
+        console.trace("err - adding user2: " + err);
+    });
+    },
+    function (err) {
+    console.trace("err - " + err);
+    });
+}
+
+
 async function main() {
-    const speech_config = sdk.SpeechConfig.fromSubscription(subscription_key, region);
-    const client = new sdk.VoiceProfileClient(speech_config);
+ 
+// function main() {
+
+    // const speech_config = sdk.SpeechConfig.fromSubscription(subscription_key, region);
+    // const client = new sdk.VoiceProfileClient(speech_config);
 
    
-    await TextIndependentVerification(client, speech_config);
-    await TextIndependentIdentification(client, speech_config);
-    console.log ("End of recognition.");
-    console.log ("Rocket Elevator Speaker Recognition");
-    speachtotext()
+    // await TextIndependentVerification(client, speech_config);
+    // await TextIndependentIdentification(client, speech_config);
+    // console.log ("End of recognition.");
+    // console.log ("Rocket Elevator Speaker Recognition");
+    // speachtotext()
+
+
+    var newProfile = await createProfile.createProfile();
+
+    console.log (newProfile);
+    transcribe();
+
+
 }
+
+
 main();
